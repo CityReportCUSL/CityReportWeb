cd2dc9d8c0a38fbd3537ef948a60756968bdbb13
Víctor Rodríguez <victornarov@gmail.com>
2020-01-06 19:04:32 +0100

    +Pagina acercade con informacion sobre el proyecto y de contacto de los desarrolladores

--- acercade.php
+++ acercade.php
@@ -1,14 +1,144 @@
 <!doctype html >
 <html lang="es">
-<body>
 <?php include 'fragmentos/cabecera.php'?> 
 
 
-<img src="wip.jpg" alt="">
-   
-<?php include 'fragmentos/pie.php'?> 
+
+    <div class="main-panel">
+
+        
+    
+        <nav class="navbar navbar-default navbar-fixed">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="acercade.php">Acerca de</a>
+                    </div>
+                    <div class="collapse navbar-collapse">
+
+                    </div>
+                </div>
+            </nav>
+        
+
+
+        <div class="content">
+            <div class="container-fluid">
+            
+
+                <div class="col-md-12">
+                    <div class="card card-user">
+                                    <div class="image">
+                                        
+                                    </div>
+                                    <div class="content">
+                                        <div class="author">
+                                            
+                                            <h4 class="title">CityReport<br />
+                                                <small>Concurso Universitario de Software Libre 2019</small>
+                                            </h4>
+                                            <div class="image" >
+                                                <img style="max-width:100%;max-height:100%;" src="https://images.fineartamerica.com/images-medium-large-5/hong-kong-city-skyline-black-and-white-vector-illustration-jpldesigns.jpg">
+                                            </div>
+                                            
+                                            
+                                        </div>
+                                        <p stile="text-align:justify;" class="description text-center" > 
+                                        <br><br>
+                                        CityReport es una herramienta que fomenta la participación ciudadana y 
+                                        facilita la comunicación con autoridades locales para reportar y gestionar incidencias.<br>
+
+                                        El objetivo es diseñar una aplicación móvil accesible y de fácil manejo que envíe los reportes a un servidor 
+                                        que los refleje en una web, de tal forma que los reportes puedan ser gestionados desde un panel 
+                                        de administración por las autoridades pertinentes.
+
+
+                                        </p>
+                                    </div>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a href="#" target="blank" class="btn btn-simple" role="button"><i class="fa fa-github"></i>Repositorio Github</a>
+                                        <a href="#" class="btn btn-simple"><i class="fa fa-wordpress" target="blank"></i>Blog</a>
+                                        <a href="mailto:cityreport@outlook.com" class="btn btn-simple"><i class="fa fa-envelope" target="blank"></i>Contacto</a>
+                                        
+
+                                    </div>
+                                </div>
+                        </div>
+
+                <div class="col-md-6">
+                            <div class="card card-user">
+                                <div class="image">
+                                    <img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
+                                </div>
+                                <div class="content">
+                                    <div class="author">
+                                        <a href="https://www.linkedin.com/in/victornarov/">
+                                        <img class="avatar border-gray" src="assets/img/default-avatar.png" alt="..."/>
+
+                                        <h4 class="title">Víctor M. Rodríguez<br />
+                                            <small>@victornarov</small>
+                                        </h4>
+                                        </a>
+                                    </div>
+                                    <p class="description text-center"> 
+                                        Estudiante de Ingeniería Informática <br/>
+                                        ETSI Universidad de Huelva
+                                    </p>
+                                </div>
+                                <hr>
+                                <div class="text-center">
+                                    <a href="mailto:victornarov@gmail.com" class="btn btn-simple" target="blank"><i class="fa fa-envelope"></i></a>
+                                    <a href="https://twitter.com/victornarov" class="btn btn-simple" target="blank"><i class="fa fa-twitter"></i></a>
+                                    <a href="https://www.linkedin.com/in/victornarov/" class="btn btn-simple" target="blank"><i class="fa fa-linkedin"></i></a>
+
+                                </div>
+                            </div>
+                    </div>
+
+
+                    <div class="col-md-6">
+                            <div class="card card-user">
+                                <div class="image">
+                                    <img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
+                                </div>
+                                <div class="content">
+                                    <div class="author">
+                                        <a href="https://www.linkedin.com/in/%C3%A1ngel-aznar-08a689184/">
+                                        <img class="avatar border-gray" src="assets/img/default-avatar.png" alt="..."/>
+
+                                        <h4 class="title">Ángel Aznar<br />
+                                            <small>@angelcuentatwit</small>
+                                        </h4>
+                                        </a>
+                                    </div>
+                                    <p class="description text-center"> 
+                                        Estudiante de Ingeniería Informática <br/>
+                                        ETSI Universidad de Huelva
+                                    </p>
+                                </div>
+                                <hr>
+                                <div class="text-center">
+                                    <a href="mailto:angelaznar.t@gmail.com" class="btn btn-simple" target="blank"><i class="fa fa-envelope"></i></a>
+                                    <a href="https://twitter.com/angelcuentatwit" class="btn btn-simple" target="blank"><i class="fa fa-twitter"></i></a>
+                                    <a href="https://www.linkedin.com/in/%C3%A1ngel-aznar-08a689184/" class="btn btn-simple" target="blank"><i class="fa fa-linkedin"></i></a>
+
+                                </div>
+                            </div>
+                    </div>
+
+    
+                </div>
+        </div>     
+
+       
+    <?php include 'fragmentos/pie.php'?> 
+
 </body>
 
 
 </html>
-
