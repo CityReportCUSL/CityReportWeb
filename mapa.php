a30feccdc87112347ac8b59da3b8e3ea41a2c1c6
Víctor Rodríguez <victornarov@gmail.com>
2020-01-11 21:39:48 +0100

    + Mostrar info reporte al hacer click
    + Al hacer click en un reporte del mapa, se desplegará una ventana mostrando una minuatura de la foto del reporte así como su información

--- mapa.php
+++ mapa.php
@@ -55,11 +55,23 @@
     L.control.scale().addTo(map);
                 
     var offset = map.getSize().x*0.15;
+    // Then move the map
     map.panBy(new L.Point(-offset, 0), {animate: false});
 
+    //var layerGroup = L.layerGroup().addTo(map);
     var markersLayer = L.featureGroup().addTo(map);
     
 
+    
+    function clickMarker(id,event)
+    {
+        
+        var zoom = map.getZoom();
+        window.location.href = "http://www.cityreport.ga/mapa.php?v1="+id+"&zoom="+zoom;
+        
+    
+        
+    }
 
     function cambiarMapa(lat, lon, idP)
     {
@@ -75,25 +87,55 @@
 
 <?php include 'funcionesphp/conexionbd.php'?>
 
+<?php
+    // comprobar si tenemos los parametros w1 y w2 en la URL
+    if (isset($_GET["v1"])) {
+    // asignar w1 y w2 a dos variables
+   
+    $zoom = $_GET["zoom"];
+    $id = $_GET["v1"];
+    $consulta2="SELECT * FROM cityreportBD.reportes where id=$id";
+    $res2=mysqli_query($conexion,$consulta2);
+    $mostrar2=mysqli_fetch_array($res2);
+    $Latitud=$mostrar2['Latitud'];
+    $Longitud=$mostrar2['Longitud'];
+    $foto=$mostrar2['foto'];
+    $nombre=$mostrar2['nombre'];
+    $estado=$mostrar2['estado'];
+    $timestamp=$mostrar2['timestamp'];
+?>
 <script>
+ $.notify({
+        // options
+        message: '<img style="width:100%;height:100%; border-radius: 20px; object-fit: cover;" src="<?php echo $foto ?>"/> <br><h2 style="text-align:center;"><?php echo $nombre ?></h2>',
+        title: '<h2 class="center-block">Reporte ID: <?php echo $id ?></h2><h3>Fecha: <?php echo $timestamp ?></h3><h3>Estado: <?php echo $estado ?></h3>',
+        icon:'' 
+    },{
+        // settings
+        type: 'danger',
+        icon_type: 'image',
+        delay: 0
 
+    });
     map.setView([<?php echo $Latitud?> ,<?php echo $Longitud?>],<?php echo $zoom ?>);    
  
     
 </script> 
 
-
 <?php
-    $consulta="SELECT * FROM reportes";
-    $res=mysqli_query($conexion,$consulta);
-
-    while($mostrar=mysqli_fetch_array($res)){
+}
 
 ?>
+<?php
+$consulta="SELECT * FROM cityreportBD.reportes";
+$res=mysqli_query($conexion,$consulta);
 
-<script>
-cambiarMapa(<?php echo $mostrar['Latitud'] ?>,<?php echo $mostrar['Longitud'] ?>,<?php echo $mostrar['id'] ?>);
-</script>
+while($mostrar=mysqli_fetch_array($res)){
+
+    ?>
+    <script>
+    cambiarMapa(<?php echo $mostrar['Latitud'] ?>,<?php echo $mostrar['Longitud'] ?>,<?php echo $mostrar['id'] ?>);
+    </script>
 
 
 <?php } ?>
