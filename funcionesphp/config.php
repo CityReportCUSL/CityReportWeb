4a8d589ab5d3e6331567b2353590292077413546
Víctor M. Rodríguez <victornarov@gmail.com>
2020-02-19 13:09:40 +0100

    +Archivo de configuración

---
+++ funcionesphp/config.php
@@ -0,0 +1,8 @@
+<?php
+    //CONFIGURACION DE LA BASE DE DATOS
+    $hostname="example.com";  //Dirección del servidor
+    $database="BD";   //Nombre de la base de datos
+    $username="user";         //Nombre de usuario de la BD
+    $password="pass";        //Contraseña 
+
+?>
