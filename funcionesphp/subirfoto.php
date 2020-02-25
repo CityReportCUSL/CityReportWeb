bb6b252f3ddab91ab7c0df7f1986b4ee38b2649d
Víctor M. Rodríguez <victornarov@gmail.com>
2020-02-25 15:33:18 +0100

    Generar y cargar miniatura de la imagen
    - Se acelera la carga de la página web al generar miniaturas de los reportes subidos de 300px de ancho.

--- funcionesphp/subirfoto.php
+++ funcionesphp/subirfoto.php
@@ -1,4 +1,5 @@
 <?PHP include 'config.php';
+	
 	//ESTABLECIMIENTO DE CONEXIÓN
 	$conexion = new mysqli($hostname,$username,$password,$database);
 	if ($conexion->connect_errno) {
@@ -17,22 +18,30 @@
 	$linea=mysqli_fetch_assoc($resultado);
 		$id=$linea['AUTO_INCREMENT'];
 	
-	$path = "../uploads/$id.jpg";
+	$path = "../uploads/$id.jpg"; 			// RUTA A LA IMAGEN SUBIDA AL SV
+	$path_min = "../uploads/$id-min.jpg"; 	//RUTA A LA MINIATURA DE LA IMAGEN
 	//SUBIR LA IMAGEN AL SERVIDOR
 	file_put_contents($path,base64_decode($foto));
 	
+	
 	//INSERCIÓN EN LA BASE DE DATOS
-	if(!$stmt=$conexion->prepare("INSERT INTO `reportes`(nombre,foto,Latitud,Longitud) VALUES (?,?,?,?)"))
+	if(!$stmt=$conexion->prepare("INSERT INTO `reportes`(nombre,foto,Latitud,Longitud, miniatura) VALUES (?,?,?,?,?)"))
 		echo "Falló la preparación de la sentencia";
 
-	if(!$stmt->bind_param("ssdd",$nombre,$path,$Latitud,$Longitud))
+	if(!$stmt->bind_param("ssdds",$nombre,$path,$Latitud,$Longitud,$path_min))
 		echo "Falló el bindeo de parametros";
 	
-	if($stmt->execute())
-		echo "ok";
-	else echo "error";
+	if(!$stmt->execute())
+		echo "error";
 
 	//CIERRE DE CONEXION
 	$stmt->close();
 	mysqli_close($conexion);
+
+	//CREACION IMAGEN EN MINIATURA PARA MOSTRAR EN EL LISTADO DE REPORTES DE LA WEB
+	include 'miniatura.php';
+	createThumbnail($path,"../uploads/$id-min.jpg",300);
+	
+	echo("ok"); //Mensaje enviado ok en la aplicacion
+	
 ?>
