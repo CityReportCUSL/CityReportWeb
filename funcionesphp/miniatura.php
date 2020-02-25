9790182a855a101f63ef7805271d6d07ba4e12d3
Víctor M. Rodríguez <victornarov@gmail.com>
2020-02-25 15:33:18 +0100

    Generar y cargar miniatura de la imagen
    - Se acelera la carga de la página web al generar miniaturas de los reportes subidos de 300px de ancho.

---
+++ funcionesphp/miniatura.php
@@ -0,0 +1,80 @@
+<?PHP
+/* 
+    Script de @pquina en Github
+    https://gist.github.com/pqina/7a42bf0833d988dd81d3c9438009da21 
+*/
+/**
+ * @param $src - a valid file location
+ * @param $dest - a valid file target
+ * @param $targetWidth - desired output width
+ * @param $targetHeight - desired output height or null
+ */
+function createThumbnail($src, $dest, $targetWidth, $targetHeight = null) {
+
+    // 1. Load the image from the given $src
+    // with the correct loader
+    $image = imagecreatefromjpeg($src);
+
+    // no image found at supplied location -> exit
+    if (!$image) {
+        return null;
+    }
+
+
+    // 2. Create a thumbnail and resize the loaded $image
+    // - get the image dimensions
+    // - define the output size appropriately
+    // - create a thumbnail based on that size
+    // - set alpha transparency for GIFs and PNGs
+    // - draw the final thumbnail
+
+    // get original image width and height
+    $width = imagesx($image);
+    $height = imagesy($image);
+
+    // maintain aspect ratio when no height set
+    if ($targetHeight == null) {
+
+        // get width to height ratio
+        $ratio = $width / $height;
+
+        // if is portrait
+        // use ratio to scale height to fit in square
+        if ($width > $height) {
+            $targetHeight = floor($targetWidth / $ratio);
+        }
+        // if is landscape
+        // use ratio to scale width to fit in square
+        else {
+            $targetHeight = $targetWidth;
+            $targetWidth = floor($targetWidth * $ratio);
+        }
+    }
+
+    // create duplicate image based on calculated target size
+    $thumbnail = imagecreatetruecolor($targetWidth, $targetHeight);
+
+    
+
+    // copy entire source image to duplicate image and resize
+    imagecopyresampled(
+        $thumbnail,
+        $image,
+        0, 0, 0, 0,
+        $targetWidth, $targetHeight,
+        $width, $height
+    );
+
+
+    // 3. Save the $thumbnail to disk
+    // - call the correct save method
+    // - set the correct quality level
+
+    // save the duplicate version of the image to disk
+    
+    imagejpeg($thumbnail,$dest,90);
+
+
+}
+
+?>
