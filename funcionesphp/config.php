6466f1f8d1124d171e309f4486e2ad1a5b5ce4fd
Víctor M. Rodríguez <victornarov@gmail.com>
2020-02-28 12:31:22 +0100

    Revert "+ Color según estado del reporte"
    This reverts commit 5229a8366004da0f48d26d9b8121682d77351d04.

--- funcionesphp/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //CONFIGURACION DE LA BASE DE DATOS
-    $hostname="52.47.189.194";  //Dirección del servidor
-    $database="cityreportBD";   //Nombre de la base de datos
-    $username="cruser";         //Nombre de usuario de la BD
-    $password="turbocr";        //Contraseña 
-
-?>
